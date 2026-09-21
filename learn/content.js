b939063bb018723f15777686d70085bcbb2817f3
rvnonty <rvnonty@gmail.com>
2026-09-21 16:29:05 +0300

    Add approved alphabet and Everyday English curriculum

---
+++ learn/content.js
@@ -0,0 +1,748 @@
+/* RVU Kids: exact approved curriculum; original audio assets not included. */
+window.RVU_CURRICULUM={
+  "source": "Ramez Alphabet Book — locked Phase 1.1, with Phase 1.2 micro-corrections",
+  "bookPages": 96,
+  "alphabet": [
+    {
+      "id": "a",
+      "letter": "A",
+      "letterName": "ay",
+      "words": [
+        "apple",
+        "ant",
+        "airplane",
+        "alligator"
+      ],
+      "sentences": [
+        "I see an apple."
+      ],
+      "pages": [
+        4,
+        5
+      ]
+    },
+    {
+      "id": "b",
+      "letter": "B",
+      "letterName": "bee",
+      "words": [
+        "ball",
+        "bear",
+        "banana",
+        "boat"
+      ],
+      "sentences": [
+        "I see a ball."
+      ],
+      "pages": [
+        6,
+        7
+      ]
+    },
+    {
+      "id": "c",
+      "letter": "C",
+      "letterName": "see",
+      "words": [
+        "cat",
+        "car",
+        "cake",
+        "cup"
+      ],
+      "sentences": [
+        "This is a cat."
+      ],
+      "pages": [
+        8,
+        9
+      ]
+    },
+    {
+      "id": "d",
+      "letter": "D",
+      "letterName": "dee",
+      "words": [
+        "dog",
+        "duck",
+        "doll",
+        "door"
+      ],
+      "sentences": [
+        "I see a dog."
+      ],
+      "pages": [
+        10,
+        11
+      ]
+    },
+    {
+      "id": "e",
+      "letter": "E",
+      "letterName": "ee",
+      "words": [
+        "elephant",
+        "egg",
+        "ear",
+        "elbow"
+      ],
+      "sentences": [
+        "This is an elephant."
+      ],
+      "pages": [
+        12,
+        13
+      ]
+    },
+    {
+      "id": "f",
+      "letter": "F",
+      "letterName": "ef",
+      "words": [
+        "fish",
+        "frog",
+        "flower",
+        "fork"
+      ],
+      "sentences": [
+        "I see a fish."
+      ],
+      "pages": [
+        14,
+        15
+      ]
+    },
+    {
+      "id": "g",
+      "letter": "G",
+      "letterName": "gee",
+      "words": [
+        "goat",
+        "grapes",
+        "gift",
+        "guitar"
+      ],
+      "sentences": [
+        "This is a goat."
+      ],
+      "pages": [
+        16,
+        17
+      ]
+    },
+    {
+      "id": "h",
+      "letter": "H",
+      "letterName": "aitch",
+      "words": [
+        "hat",
+        "house",
+        "horse",
+        "hand"
+      ],
+      "sentences": [
+        "I see a hat."
+      ],
+      "pages": [
+        18,
+        19
+      ]
+    },
+    {
+      "id": "i",
+      "letter": "I",
+      "letterName": "eye",
+      "words": [
+        "island",
+        "iguana",
+        "ice cream",
+        "insect"
+      ],
+      "sentences": [
+        "I see an iguana."
+      ],
+      "pages": [
+        20,
+        21
+      ]
+    },
+    {
+      "id": "j",
+      "letter": "J",
+      "letterName": "jay",
+      "words": [
+        "juice",
+        "jacket",
+        "jellyfish",
+        "jam"
+      ],
+      "sentences": [
+        "This is a jacket."
+      ],
+      "pages": [
+        22,
+        23
+      ]
+    },
+    {
+      "id": "k",
+      "letter": "K",
+      "letterName": "kay",
+      "words": [
+        "kite",
+        "key",
+        "kitten",
+        "kangaroo"
+      ],
+      "sentences": [
+        "I see a kite."
+      ],
+      "pages": [
+        24,
+        25
+      ]
+    },
+    {
+      "id": "l",
+      "letter": "L",
+      "letterName": "el",
+      "words": [
+        "lion",
+        "lemon",
+        "leaf",
+        "lamp"
+      ],
+      "sentences": [
+        "This is a lion."
+      ],
+      "pages": [
+        26,
+        27
+      ]
+    },
+    {
+      "id": "m",
+      "letter": "M",
+      "letterName": "em",
+      "words": [
+        "moon",
+        "milk",
+        "monkey",
+        "mouse"
+      ],
+      "sentences": [
+        "This is a monkey."
+      ],
+      "pages": [
+        28,
+        29
+      ]
+    },
+    {
+      "id": "n",
+      "letter": "N",
+      "letterName": "en",
+      "words": [
+        "nest",
+        "nose",
+        "net",
+        "night"
+      ],
+      "sentences": [
+        "I see a nest."
+      ],
+      "pages": [
+        30,
+        31
+      ]
+    },
+    {
+      "id": "o",
+      "letter": "O",
+      "letterName": "oh",
+      "words": [
+        "orange",
+        "octopus",
+        "ocean",
+        "otter"
+      ],
+      "sentences": [
+        "I see an octopus."
+      ],
+      "pages": [
+        32,
+        33
+      ]
+    },
+    {
+      "id": "p",
+      "letter": "P",
+      "letterName": "pee",
+      "words": [
+        "pig",
+        "pen",
+        "pizza",
+        "panda"
+      ],
+      "sentences": [
+        "This is a panda."
+      ],
+      "pages": [
+        34,
+        35
+      ]
+    },
+    {
+      "id": "q",
+      "letter": "Q",
+      "letterName": "cue",
+      "words": [
+        "queen",
+        "quilt",
+        "quail"
+      ],
+      "sentences": [
+        "This is a queen."
+      ],
+      "pages": [
+        36,
+        37
+      ]
+    },
+    {
+      "id": "r",
+      "letter": "R",
+      "letterName": "ar",
+      "words": [
+        "rabbit",
+        "robot",
+        "rain",
+        "rainbow"
+      ],
+      "sentences": [
+        "I see a rainbow."
+      ],
+      "pages": [
+        38,
+        39
+      ]
+    },
+    {
+      "id": "s",
+      "letter": "S",
+      "letterName": "ess",
+      "words": [
+        "sun",
+        "star",
+        "sock",
+        "spoon"
+      ],
+      "sentences": [
+        "This is a star."
+      ],
+      "pages": [
+        40,
+        41
+      ]
+    },
+    {
+      "id": "t",
+      "letter": "T",
+      "letterName": "tee",
+      "words": [
+        "tiger",
+        "train",
+        "tree",
+        "turtle"
+      ],
+      "sentences": [
+        "I see a train."
+      ],
+      "pages": [
+        42,
+        43
+      ]
+    },
+    {
+      "id": "u",
+      "letter": "U",
+      "letterName": "you",
+      "words": [
+        "umbrella",
+        "uncle",
+        "up",
+        "under"
+      ],
+      "sentences": [
+        "This is an umbrella."
+      ],
+      "pages": [
+        44,
+        45
+      ]
+    },
+    {
+      "id": "v",
+      "letter": "V",
+      "letterName": "vee",
+      "words": [
+        "van",
+        "vase",
+        "violin",
+        "vegetables"
+      ],
+      "sentences": [
+        "This is a van."
+      ],
+      "pages": [
+        46,
+        47
+      ]
+    },
+    {
+      "id": "w",
+      "letter": "W",
+      "letterName": "double-you",
+      "words": [
+        "whale",
+        "watermelon",
+        "window",
+        "watch"
+      ],
+      "sentences": [
+        "I see a whale."
+      ],
+      "pages": [
+        48,
+        49
+      ]
+    },
+    {
+      "id": "x",
+      "letter": "X",
+      "letterName": "ex",
+      "words": [
+        "x-ray",
+        "xylophone"
+      ],
+      "sentences": [
+        "This is an x-ray.",
+        "This is a xylophone."
+      ],
+      "pages": [
+        50,
+        51
+      ]
+    },
+    {
+      "id": "y",
+      "letter": "Y",
+      "letterName": "why",
+      "words": [
+        "yo-yo",
+        "yogurt",
+        "yellow",
+        "yak"
+      ],
+      "sentences": [
+        "This is a yo-yo."
+      ],
+      "pages": [
+        52,
+        53
+      ]
+    },
+    {
+      "id": "z",
+      "letter": "Z",
+      "letterName": "zee",
+      "words": [
+        "zebra",
+        "zoo",
+        "zipper",
+        "zero"
+      ],
+      "sentences": [
+        "This is a zebra."
+      ],
+      "pages": [
+        54,
+        55
+      ]
+    }
+  ],
+  "sentences": [
+    {
+      "id": "feelings",
+      "title": "My Feelings",
+      "titleAr": "مشاعري",
+      "sentences": [
+        "I'm happy.",
+        "I'm sad.",
+        "I'm angry.",
+        "I'm scared."
+      ],
+      "words": [],
+      "pages": [
+        56,
+        57
+      ]
+    },
+    {
+      "id": "hungry",
+      "title": "I'm Hungry",
+      "titleAr": "أنا جعان",
+      "sentences": [
+        "I'm hungry.",
+        "Can I have a snack, please?"
+      ],
+      "words": [
+        "apple",
+        "banana",
+        "pizza"
+      ],
+      "pages": [
+        58,
+        59
+      ]
+    },
+    {
+      "id": "thirsty",
+      "title": "I'm Thirsty",
+      "titleAr": "أنا عطشان",
+      "sentences": [
+        "I'm thirsty.",
+        "Can I have some water, please?"
+      ],
+      "words": [
+        "cup",
+        "juice",
+        "milk"
+      ],
+      "pages": [
+        60,
+        61
+      ]
+    },
+    {
+      "id": "bathroom",
+      "title": "I Need the Bathroom",
+      "titleAr": "عاوز أدخل الحمام",
+      "sentences": [
+        "I need to go to the bathroom.",
+        "Can I go to the bathroom, please?",
+        "I wash my hands."
+      ],
+      "words": [
+        "bathroom",
+        "wash",
+        "hands"
+      ],
+      "pages": [
+        62,
+        63
+      ]
+    },
+    {
+      "id": "my-family",
+      "title": "My Family",
+      "titleAr": "عيلتي",
+      "sentences": [
+        "This is my family.",
+        "This is my mom.",
+        "This is my dad.",
+        "This is my brother.",
+        "This is my sister."
+      ],
+      "words": [
+        "family",
+        "mom",
+        "dad",
+        "brother",
+        "sister"
+      ],
+      "pages": [
+        64,
+        65
+      ]
+    },
+    {
+      "id": "hello-about-me",
+      "title": "Hello & About Me",
+      "titleAr": "التعارف",
+      "sentences": [
+        "Hello!",
+        "My name is ______.",
+        "What's your name?",
+        "Goodbye!"
+      ],
+      "words": [],
+      "pages": [
+        66,
+        67
+      ]
+    },
+    {
+      "id": "at-school",
+      "title": "At School",
+      "titleAr": "في المدرسة",
+      "sentences": [
+        "I'm at school.",
+        "This is my teacher.",
+        "I have a book.",
+        "I have a pencil."
+      ],
+      "words": [
+        "teacher",
+        "book",
+        "pencil",
+        "school"
+      ],
+      "pages": [
+        68,
+        69
+      ]
+    },
+    {
+      "id": "can-i-have",
+      "title": "Can I Have…?",
+      "titleAr": "الطلب بطريقة مهذبة",
+      "sentences": [
+        "Can I have _____, please?",
+        "Can I have the ball, please?",
+        "Can I have the book, please?",
+        "Can I have some water, please?"
+      ],
+      "words": [
+        "apple",
+        "ball",
+        "book",
+        "juice",
+        "water"
+      ],
+      "pages": [
+        70,
+        71
+      ]
+    },
+    {
+      "id": "likes-and-dislikes",
+      "title": "I Like / I Don't Like",
+      "titleAr": "بحب ومش بحب",
+      "sentences": [
+        "I like pizza.",
+        "I like bananas.",
+        "I don't like milk.",
+        "I don't like yogurt."
+      ],
+      "words": [],
+      "pages": [
+        72,
+        73
+      ]
+    },
+    {
+      "id": "can-you-help",
+      "title": "Can You Help Me?",
+      "titleAr": "ممكن تساعدني؟",
+      "sentences": [
+        "Can you help me, please?",
+        "I don't understand.",
+        "Say it again, please.",
+        "Please open the box.",
+        "Please close the door."
+      ],
+      "words": [],
+      "pages": [
+        74,
+        75
+      ]
+    },
+    {
+      "id": "polite-words",
+      "title": "Please / Thank You / I'm Sorry",
+      "titleAr": "كلمات مهذبة",
+      "sentences": [
+        "Please.",
+        "Thank you.",
+        "I'm sorry.",
+        "Excuse me.",
+        "You're welcome."
+      ],
+      "words": [],
+      "pages": [
+        76,
+        77
+      ]
+    },
+    {
+      "id": "lets-play",
+      "title": "Let's Play",
+      "titleAr": "يلا نلعب",
+      "sentences": [
+        "Let's play.",
+        "Can I play with you?",
+        "My turn.",
+        "Your turn.",
+        "Let's share."
+      ],
+      "words": [],
+      "pages": [
+        78,
+        79
+      ]
+    },
+    {
+      "id": "my-body",
+      "title": "My Body & How I Feel",
+      "titleAr": "جسمي وإحساسي",
+      "sentences": [
+        "My head hurts.",
+        "My tummy hurts.",
+        "My hand hurts.",
+        "I'm tired.",
+        "I'm sick."
+      ],
+      "words": [],
+      "pages": [
+        80,
+        81
+      ]
+    },
+    {
+      "id": "daily-routine",
+      "title": "My Daily Routine",
+      "titleAr": "روتيني اليومي",
+      "sentences": [
+        "I wake up.",
+        "I brush my teeth.",
+        "I get dressed.",
+        "I eat breakfast.",
+        "I go to school.",
+        "I go to bed."
+      ],
+      "words": [],
+      "pages": [
+        82,
+        83
+      ]
+    },
+    {
+      "id": "stay-safe",
+      "title": "Safety Words & Basic Needs",
+      "titleAr": "كلام يحافظ على سلامتي",
+      "sentences": [
+        "Stop.",
+        "Wait.",
+        "No, thank you.",
+        "Help me, please.",
+        "I need my mom.",
+        "I need my dad."
+      ],
+      "words": [],
+      "pages": [
+        84,
+        85
+      ]
+    }
+  ]
+};
